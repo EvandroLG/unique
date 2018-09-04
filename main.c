bb1e50a25735d03351e6ee242259510f4979110a
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-09-04 11:44:13 +0100

    alittle refactoring - create push function and include stdbool

--- main.c
+++ main.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+void push(int array[], int *index, int value) {
+    array[*index] = value;
+    *index = *index + 1;
+}
 
 int unique(int array[], int size) {
     int *output = malloc(size * sizeof(int));
     int output_index = 0;
-    int current_value, has_value;
+    int current_value;
+    bool has_value;
 
     for (int i = 0; i < size; i++) {
         current_value = array[i];
-        has_value = 0;
+        has_value = false;
 
         for (int j = 0; j < size; j++) {
             if (current_value == output[j]) {
-               has_value = 1; 
+               has_value = true;
             }
         }
 
         if (has_value == 0) {
-            output[output_index] = current_value;
-            output_index = output_index + 1;
+            push(output, &output_index, current_value);
         }
     }
 
